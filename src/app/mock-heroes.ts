a287a13a55f3ec6bca0c32af8fc4fe072c613362
KIZAFOX <victor.pernak@hotmail.fr>
2021-07-30 11:27:27 +0200

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Cabochard'},
+    { id: 2, name: 'Cinkrof'},
+    { id: 3, name: 'SAKEN'},
+    { id: 4, name: 'xMaty'},
+    { id: 5, name: 'Targamas'},
+    { id: 6, name: 'Kameto'},
+    { id: 7, name: 'Prime'},
+    { id: 8, name: 'Shanky'},
+    { id: 9, name: 'Striker'},
+];
